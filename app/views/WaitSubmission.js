bdb56bc78a556021da7f3b24f23323ade8df84bf
benjamin-musil <benjamin.musil@gmail.com>
2019-11-18 22:22:17 -0600

    Added camera imports and new capture button

--- app/views/WaitSubmission.js
+++ app/views/WaitSubmission.js
@@ -1,12 +1,34 @@
 import React,  { Component } from 'react';
-import {StyleSheet, ScrollView, View, Text, TextInput, Button, Image} from 'react-native';
+import {StyleSheet, ScrollView, View, Text, TextInput, Button, Image, Alert} from 'react-native';
 import {Table, Row, Rows} from 'react-native-table-component';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import moment from 'moment';
+import Geolocation from '@react-native-community/geolocation';
+import { RNCamera } from 'react-native-camera';
 
 export default class WaitSubmission extends React.Component {
+  // Title of view for navigation
   static navigationOptions = {
     title: 'WaitSubmission'
+  };
+
+  // Initiate state for geolocation
+  // https://github.com/react-native-community/react-native-geolocation
+  state = {
+    location: null
+  }
+
+  // Function to get coordinates in JSON format
+  findCoordinates = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const location = JSON.stringify(position);
+
+        this.setState({ location });
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000} // maximumAge: 1000, might need for iOS
+    );
   };
 
   render() {
@@ -20,8 +42,14 @@ export default class WaitSubmission extends React.Component {
               />
         </ScrollView>
           <Button
-            title="Submit"
+            title="Snag a Pic"
+            onPress={this.findCoordinates}
           />
+          <Button
+            title="Submit"
+            onPress={this.findCoordinates}
+          />
+          <Text>Location: {this.state.location}</Text>
       </View>
     );
   }
